dfc5c59b257e6cc27f66faa17e1146e944115c54
giho <seonglf@rebeltech.info>
2024-01-22 20:55:41 +0900

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+export const app = express();
+const port = 3001;
+
+/* front 주소 */
+const corsOptions = {
+    origin: "http://localhost:3000"
+};
+
+app.use(cors(corsOptions));
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
